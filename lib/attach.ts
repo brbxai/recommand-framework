8c1485e87fb5d9c9e67999685910deaef0fa57ff
Daan Lenaerts <me@daanlenaerts.com>
2025-05-10 08:55:54 +0200

    Support for public files and index.html overrides

--- lib/attach.ts
+++ lib/attach.ts
@@ -2,14 +2,39 @@ import type { Hono } from "hono";
 import { join } from "node:path";
 import { type RecommandApp } from "./app";
 import { frameworkLogger } from "./logger";
-export async function attach(app: RecommandApp, hono: Hono): Promise<void> {
-    frameworkLogger.info(`Loading ${app.name} from ${app.absolutePath}`);
-    const appModule = await import(join(app.absolutePath, "index.ts"));
-    await appModule.init(app, hono);
-    try{
-        hono.route("api/" + app.name, appModule.default);
-    }catch(e){
-        frameworkLogger.error(`Failed to register api routes for ${app.name}`);
+import { serveStatic } from "hono/bun";
+import { existsSync, readdirSync } from "node:fs";
+
+export async function attach(
+  app: RecommandApp,
+  hono: Hono
+): Promise<{ indexOverride: string | null }> {
+  frameworkLogger.info(`Loading ${app.name} from ${app.absolutePath}`);
+  const appModule = await import(join(app.absolutePath, "index.ts"));
+  await appModule.init(app, hono);
+  try {
+    hono.route("api/" + app.name, appModule.default);
+  } catch (e) {
+    frameworkLogger.error(`Failed to register api routes for ${app.name}`);
+  }
+
+  // For each file in the public folder, register a root route
+  let indexOverride: string | null = null;
+  const publicPath = join(app.absolutePath, "public");
+  if (existsSync(publicPath)) {
+    const files = readdirSync(publicPath);
+
+    // Check for index.html and set it as the indexOverride
+    if (files.includes("index.html")) {
+      indexOverride = join(publicPath, "index.html");
     }
-    frameworkLogger.info(`${app.name} is loaded`);
+
+    files.forEach((file: string) => {
+      hono.get(`/${file}/*`, serveStatic({ root: publicPath }));
+    });
+  }
+
+  frameworkLogger.info(`${app.name} is loaded`);
+
+  return { indexOverride };
 }
