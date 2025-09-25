b2b8aff577f5c61544fd3e58fc76b80a9650f68b
Daan Lenaerts <me@daanlenaerts.com>
2025-09-25 23:01:43 +0200

    Include invalid input details

--- lib/zod-validator.ts
+++ lib/zod-validator.ts
@@ -12,7 +12,43 @@ export function zodValidator<
 ) {
     return baseZValidator(target, schema, (result, c) => {
         if (!result.success) {
-            return c.json(actionFailure(result.error), 400);
+            return c.json({
+                errors: {
+                    ...actionFailure(result.error).errors,
+                    invalidInputDetails: cleanZodError(result.error),
+                },
+            }, 400);
         }
     });
 }
+
+const cleanZodError = (error: z.ZodError): any[] => {
+    // Output the error in a more readable format with path and message for each error
+    const errorsArray = [];
+    for (const issue of error.issues) {
+        if (issue.code === 'invalid_union') {
+            const outerErrors = [];
+            for (const unionError of issue.unionErrors) {
+                const innerErrors = [];
+                for (const unionErrorIssue of unionError.issues) {
+                    innerErrors.push({
+                        path: unionErrorIssue.path.join('.'),
+                        message: unionErrorIssue.message,
+                    });
+                }
+                outerErrors.push(innerErrors);
+            }
+            errorsArray.push({
+                path: issue.path.join('.'),
+                message: "Invalid union, fix at least one of the following errors depending on the input type you are targeting.",
+                unionErrors: outerErrors,
+            });
+        }else{
+            errorsArray.push({
+                path: issue.path.join('.'),
+                message: issue.message,
+            });
+        }
+    }
+    return errorsArray;
+};
